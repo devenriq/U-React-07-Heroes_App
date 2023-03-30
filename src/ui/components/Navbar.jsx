7a98c150b5fca7894d38719ea07de55d177fa5e5
enrique palomino <octepc.97@gmail.com>
2023-03-30 14:55:47 -0500

    feat: :sparkles: Added useNavigate hook to route the pages

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,12 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/login");
+  };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -29,22 +35,13 @@ export const Navbar = () => {
         </div>
       </div>
 
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-        <ul className="navbar-nav ml-auto">
-          <NavLink
-            className={({ isActive }) =>
-              `nav-link nav-item ${isActive ? "active" : ""}`
-            }
-            to="/login"
-          >
-            Logout
-          </NavLink>
-        </ul>
-      </div>
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2"></div>
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-primary">Enrique</span>
-          <button className="nav-item nav-link btn">Logout</button>
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
+            Logout
+          </button>
         </ul>
       </div>
     </nav>
