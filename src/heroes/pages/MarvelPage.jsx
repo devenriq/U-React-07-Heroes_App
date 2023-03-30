591d26fe52ad9ba8a3f0977a02d32ef4dd9e0341
enrique palomino <octepc.97@gmail.com>
2023-03-30 15:41:10 -0500

    feat: :sparkles: Added a data fil which is consumed to render info to the client view

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,3 +1,11 @@
+import { HeroList } from "../components/HeroList";
+
 export const MarvelPage = () => {
-  return <h1>MarvelPage</h1>;
+  return (
+    <>
+      <h1>MarvelPage</h1>
+
+      <HeroList publisher={"Marvel Comics"} />
+    </>
+  );
 };
