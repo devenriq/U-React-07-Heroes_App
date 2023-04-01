3851eadaeed733ca66e4e9405ef5c544f38672be
enrique palomino <octepc.97@gmail.com>
2023-04-01 10:54:38 -0500

    fix: Found a bug related with the url

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import { getHeroById } from "../helpers/getHeroById";
 
 export const HeroPage = () => {
+  const { id } = useParams();
+
+  const hero = getHeroById(id);
+
+  console.log(id);
+
   return (
     <>
       <h1>Hero Component</h1>
